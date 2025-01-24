c453df337f71cdcf94786dfbd594a5586a4f114f
itsLydt <lklaus.dev@gmail.com>
2025-01-24 14:41:17 -0600

    initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * GraphicalLCD.c
+ *
+ * Created: 1/24/2025 2:38:50 PM
+ * Author : itsLydt
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
