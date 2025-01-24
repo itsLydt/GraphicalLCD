951167e2f4a0c210bf4ba2fb2059dbb36f302a0a
itsLydt <lklaus.dev@gmail.com>
2025-01-24 16:14:21 -0600

    gpio init for LCD

--- lcd.c
+++ lcd.c
@@ -5,8 +5,14 @@
  *  Author: itsLydt
  */ 
 
+#include "lcd.h"
 #include "ATSAMD21/drivers/gpio/gpio.h"
+
+const uint8_t DB_MASK = 0xFF;
+const uint8_t CTRL_MASK = (1 << PIN_RS) | (1 << PIN_RW) | (1 << PIN_E);
 
 void LCD_Init(){
 	// configure GPIO pins that will be used to control the display
+	GPIO_SetPortDirection(DATA_PORT, DB_MASK << PIN_DB0, GPIO_OUT);
+	GPIO_SetPortDirection(CTRL_PORT, CTRL_MASK, GPIO_OUT);
 }
--- lcd.h
+++ lcd.h
@@ -11,6 +11,14 @@
 #ifndef LCD_H_
 #define LCD_H_
 
+extern const PortGroup* const CTRL_PORT;
+extern const uint8_t PIN_RS;
+extern const uint8_t PIN_RW;
+extern const uint8_t PIN_E;
+
+extern const PortGroup* const DATA_PORT;
+extern const uint8_t PIN_DB0;
+
 //init LCD
 void LCD_Init();
 
